91972b30e2dc822d229480cbdf44e0de0e6b94a6
Munkh-AmgalanDav <haay.maay@gmail.com>
2025-03-18 21:58:52 -0400

    Prepared backend-food for deployment

--- backend-food/server.js
+++ backend-food/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
-app.use(express.json());
+// Use process.env.PORT for dynamic port assignment
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello from Express on Render!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
